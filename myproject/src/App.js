c7201d6db034c23cd36e396e16fd9d69c3a39c62
seunghyeonKang <number42isuser@gmail.com>
2024-07-25 11:58:54 +0900

    chore: reset css 및 styled-component 설치

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,10 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import "./App.css";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 
 function App() {
   return (
-    <>
-      <div className="App"></div>;
-    </>
+    <div className="App">
+      <GlobalStyle />
+      <h3>dddd</h3>
+      <h3>dddd</h3>
+      <h3>dddd</h3>
+    </div>
   );
 }
 

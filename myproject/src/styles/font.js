bfc05ce7dae73d12b0aa37caa52adac142cb0231
seunghyeonKang <number42isuser@gmail.com>
2024-08-06 19:42:40 +0900

    chore: index.html 기본 스타일 수정(font-size: 10px; 설정 등)

--- myproject/src/styles/font.js
+++ myproject/src/styles/font.js
@@ -8,11 +8,13 @@ function FONT({ weight, size }) {
 
 const font = {
   // 10px = 1rem
-  //   (예시) thin, extralight, light, regular, medium, semibold, bold, extrabold, black
+  //   (예시) thin(1), extralight(2), light(3), regular(4), medium(5), semibold(6), bold(7), extrabold(8), black(9)
   //   title: FONT({ weight: 300, size: 1.7 }),
   //   regular_10: FONT({ weight: 400, size: 1.0 }),
   //   base_16: FONT({ weight: 600, size: 1.6 }),
+  regular_16: FONT({ weight: 400, size: 1.6 }),
   semibold_20: FONT({ weight: 600, size: 2.0 }),
+  test_100: FONT({ weight: 900, size: 10.0 }),
 };
 
 export default font;

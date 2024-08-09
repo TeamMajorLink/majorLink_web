3bdb0880d5ee1d0c0b99dbff8ec2161a6a32c23a
seunghyeonKang <number42isuser@gmail.com>
2024-08-10 00:41:05 +0900

    design: 검색바 이미지 수정

--- myproject/src/components/class/search/SearchSection.jsx
+++ myproject/src/components/class/search/SearchSection.jsx
@@ -1,10 +1,11 @@
 // import { createGlobalStyle } from "styled-components";
-import { styled } from 'styled-components';
+import { styled, css } from 'styled-components';
 import color from '../../../styles/color';
 import font from '../../../styles/font';
 import { SearchBar } from './SearchBar';
-import BackgroundPng from '../../../assets/class/class_background_1920x316.png';
-import CharacterPng from '../../../assets/class/class_character_1920x316.png';
+import BackgroundLeftPng from '../../../assets/class/class_background_planet_68x316.png';
+import BackgroundRightPng from '../../../assets/class/class_background_plane_600x316.png';
+import CharacterPng from '../../../assets/class/class_character_176x202.png';
 
 const SearchContainer = styled.div`
   margin: 0;
@@ -13,23 +14,38 @@ const SearchBackground = styled.div`
   height: 316px;
   background: ${() => color.gradation_primary_class};
 `;
-const BackgroundContainer = styled.div`
+const BackgroundContainer = css`
   margin: -316px 0 0 0;
   padding: 0;
   height: 316px;
 
   display: flex;
   justify-content: center;
+  align-items: center;
 
   overflow: hidden;
+`;
+const BackgroundContainerLeft = styled.div`
+  ${BackgroundContainer}
+  padding: 0 calc(97vw - 64px) 0 calc(7vw - 4px);
+`;
+const BackgroundContainerRight = styled.div`
+  ${BackgroundContainer}
+  padding: 0 calc(4vw - 24px) 0 calc(96vw - 576px);
+`;
+const BackgroundContainerCharacter = styled.div`
+  ${BackgroundContainer}
+  padding: 0 calc(14vw - 24px) 0 calc(86vw - 152px);
 `;
 const BackgroundImg = styled.img`
   height: 100%;
   object-fit: cover;
 `;
+const CharacterImg = styled.img`
+  height: 202px;
+  object-fit: cover;
+`;
 const ContentContainer = styled.div`
-  box-sizing: content-box;
-
   margin: -316px 0 0 0;
   padding: 0 calc(78vw - 648px) 0 calc(22vw - 184px);
   width: 831px;
@@ -50,12 +66,15 @@ export function SearchSection() {
   return (
     <SearchContainer>
       <SearchBackground />
-      <BackgroundContainer>
-        <BackgroundImg src={BackgroundPng} alt="img" />
-      </BackgroundContainer>
-      <BackgroundContainer>
-        <BackgroundImg src={CharacterPng} alt="img" />
-      </BackgroundContainer>
+      <BackgroundContainerLeft>
+        <BackgroundImg src={BackgroundLeftPng} alt="img" />
+      </BackgroundContainerLeft>
+      <BackgroundContainerRight>
+        <BackgroundImg src={BackgroundRightPng} alt="img" />
+      </BackgroundContainerRight>
+      <BackgroundContainerCharacter>
+        <CharacterImg src={CharacterPng} alt="img" />
+      </BackgroundContainerCharacter>
       <ContentContainer>
         <PageSubName>클래스 찾기</PageSubName>
         <SearchBar />

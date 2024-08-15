4b17bd8e0e23499bda0cc5e1aa24442ed99adf2f
moonseo <moonsh0913@gmail.com>
2024-08-15 19:42:52 +0900

    setting

---
+++ myproject/src/pages/OfferClass.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+// import HeaderComponent from '../components/common/header/HeaderComponent';
+
+const Profile = styled.div`
+
+`;
+
+function OfferClass() {
+    return (
+        <>
+            {/* <HeaderComponent /> */}
+            <Profile>
+                test
+            </Profile>
+        </>
+    );
+}
+
+export default OfferClass
---
+++ myproject/src/pages/ProfileCardMake.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+// import HeaderComponent from '../components/common/header/HeaderComponent';
+
+const Profile = styled.div`
+
+`;
+
+function ProfileCardMake() {
+    return (
+        <>
+            {/* <HeaderComponent /> */}
+            <Profile>
+                test
+            </Profile>
+        </>
+    );
+}
+
+export default ProfileCardMake

796c3459aff01d799ebf3d139d1d131cda64b2cd
moonseo <moonsh0913@gmail.com>
2024-08-21 11:00:20 +0900

    lecture_page_start

--- myproject/src/pages/users/ProfileCardMake.jsx
+++ myproject/src/pages/users/ProfileCardMake.jsx
@@ -14,15 +14,45 @@ const Container = styled.div`
 `;
 
 const ProfileContainer = styled.div`
-  // display: flex;
-  margin: 29.7rem 19.8rem 0 8.4rem;
+  // margin: 29.7rem 19.8rem 0 8.4rem;
   // margin-right(8.4): 임의
+  padding: 16.2rem 25.7rem; //bottom, right: 임의
 
 `;
 
 const Subhead = styled.p`
   font: ${() => font.semibold_24};
   color: ${() => color.black};
+  margin: 4.8rem 0 3.3rem 5.3rem;
+`;
+
+const BigInput = styled.input`
+  width: 105.5rem;
+  height: auto;
+  padding: 2rem 2.7rem;
+  font: ${() => font.regular_20};
+  color: ${() => color.black};
+  border: 0.1rem solid ${() => color.grayscale_9d};
+  border-radius: 1.5rem;
+
+`;
+
+// 수정하기
+const BasicInput = styled.input`
+  height: 6.6rem;
+  padding: 2.5rem 4.4rem;
+  font: ${() => font.regular_20};
+  color: ${() => color.black};
+  border: 0.1rem solid ${() => color.grayscale_9d};
+  border-radius: 1.5rem;
+  // 높이: 정중앙에 정렬하는 거 뭐더라
+
+  .short{
+    width: 56.5rem;
+  }
+  .long{
+    width: 65.6rem;
+  }
 `;
 
 const CharacterCount = styled.p`
@@ -89,22 +119,27 @@ function ProfileCardMake() {
             <RectangleProfile />
 
             <Subhead>한 줄 소개</Subhead>
+            <BigInput placeholder='한 줄 소개를 입력해주세요' />
             {/* 한 줄 소개를 입력해주세요 */}
+            <CharacterCount>0/100</CharacterCount>
 
 
 
             <ProfileTitle title='자기소개'/>
             <Subhead>자기소개</Subhead>
+            <BigInput placeholder='직무 경험, 핵심 역량, 강점 등 구체적인 내용을 작성해보세요.' />
             {/* 직무 경험, 핵심 역량, 강점 등 구체적인 내용을 작성해보세요. */}
             <CharacterCount>0/1000</CharacterCount>
 
             <ProfileTitle title='학력 및 교육' />
             <Container>
               <Subhead>교육기관</Subhead>
+              <BasicInput className='short' placeholder='학교명이나 교육기관 명을 입력해주세요' />
               {/* 학교명이나 교육기관 명을 입력해주세요 */}
             </Container>
             <Container>
               <Subhead>전공/과정</Subhead>
+              <BasicInput className='long' placeholder='전공 혹은 과정 명을 입력해주세요. 예) 컴퓨터학과/웹 개발 과정' />
               {/* 전공 혹은 과정 명을 입력해주세요. 예) 컴퓨터학과/웹 개발 과정 */}
             </Container>
             <Container> {/* className 다르게 줘서 flex 별도 설정 */}
@@ -116,10 +151,12 @@ function ProfileCardMake() {
             <ProfileTitle title='활동 프로젝트' />
             <Container>
               <Subhead>활동 구분</Subhead>
+              <BasicInput className='short' placeholder='예) 인턴, 대외활동, 프로젝트, 동아리 등' />
               {/* 예) 인턴, 대외활동, 프로젝트, 동아리 등 */}
             </Container>
             <Container>
               <Subhead>기관/장소</Subhead>
+              <BasicInput className='long' placeholder='기관/장소 명을 입력해주세요' />
               {/* 기관/장소 명을 입력해주세요 */}
             </Container>
             <Container>
@@ -128,6 +165,7 @@ function ProfileCardMake() {
             </Container>
             <Container>
               <Subhead>활동 내용</Subhead>
+              <BasicInput className='long' placeholder='예) ios 모바일 어플 개발 프로젝트 진행' />
               {/* 예) ios 모바일 어플 개발 프로젝트 진행 */}
             </Container>
             {/* 추가하기 버튼 컴포넌트? */}
@@ -135,7 +173,8 @@ function ProfileCardMake() {
             <ProfileTitle title='보유 기술' />
             <Container>
               <Subhead>보유 기술</Subhead>
-              {/* 키워드 입력 후 Enter */}
+              <BasicInput className='long' placeholder='키워드 입력 후 Enter' />
+              {/* 수정 - Enter 또는 버튼 클릭 시 기능 추가하기 */}
               {/* 등록 버튼 컴포넌트? */}
               <Text>예시: Google Ananlytics, Photoshop, Figma, Notion, Python, Javascript, Confluence 등</Text>
               <TagContainer>
@@ -147,12 +186,14 @@ function ProfileCardMake() {
             <ProfileTitle title='포트폴리오' />
             <Container>
               <Subhead>첨부파일</Subhead>
+              <BasicInput type='file' className='long' placeholder='첨부할 파일을 찾아주세요'/>
               {/* 첨부할 파일을 찾아주세요 */}
               {/* 파일 첨부 버튼 컴포넌트 */}
             </Container>
 
             <Container>
               <Subhead>URL</Subhead>
+              <BasicInput type='url' className='long' placeholder='URL 링크를 입력하세요' />
               {/* URL 링크를 입력하세요 */}
               {/* 등록 버튼 컴포넌트 */}
               <UrlContainer>

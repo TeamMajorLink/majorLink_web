9d6b5a471ef1496818a60e5b87b0ee27875476b0
seunghyeonKang <number42isuser@gmail.com>
2024-08-22 02:41:36 +0900

    연동중: 알림 실패,, 머지용 분리 수정

--- myproject/src/components/common/header/HeaderAfterLogin.jsx
+++ myproject/src/components/common/header/HeaderAfterLogin.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
-// import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-// import axios from 'axios';
 import * as A from './Header.Style';
 import { HeaderNavigationBar } from './HeaderNavigationBar';
 import BookmarkIconPng from '../../../assets/common/header_icon_bookmark_30x30.png';
@@ -9,84 +7,8 @@ import MessageIconPng from '../../../assets/common/header_icon_message_30x30.png
 import AlarmIconPng from '../../../assets/common/header_icon_alarm_30x30.png';
 import ProgilePng from '../../../assets/common/back_header_profile_63x63.png';
 
-export function HeaderAfterLogin({ authToken }) {
+export function HeaderAfterLogin() {
   const navigate = useNavigate();
-  // const [notifications, setNotifications] = useState([]);
-  // console.log(authToken(헤더 알림): ${authToken});
-
-  // 브라우저 알림 표시
-  const showNotification = (notification) => {
-    if (!notification) return; // notification 데이터가 없으면 리턴
-
-    const browserNotification = new Notification(
-      'HeaderAfterLogin.jsx에서 보내는 알림',
-      {
-        body: notification.content, // 알림 내용 표시
-      },
-    );
-
-    setTimeout(() => {
-      browserNotification.close();
-    }, 10 * 1000);
-
-    browserNotification.addEventListener('click', () => {
-      window.open(notification.url, '_blank'); // 알림 클릭 시 링크 열기
-    });
-  };
-
-  // 알림 연동
-  useEffect(() => {
-    const fetchNotifications = () => {
-      // WebSocket을 사용하여 실시간 알림 처리
-      const socket = new EventSource(
-        `https://dev.majorlink.store/notification/subscribe/${authToken}`,
-      );
-
-      socket.onmessage = (event) => {
-        const newNotification = JSON.parse(event.data);
-        // 초기 연결 메시지를 무시하도록 필터링(오류 원인찾기용)
-        // 초기 연결 메시지를 무시하도록 필터링(오류 원인찾기용)
-        if (
-          typeof newNotification === 'string' &&
-          newNotification.includes('EventStream Create')
-        ) {
-          console.log('알림 없다고요~~');
-          return;
-        }
-        console.log(`소켓 데이터: ${newNotification.content}`);
-
-        // 브라우저 알림 표시
-        showNotification(newNotification);
-      };
-
-      socket.onerror = (err) => {
-        console.error('WebSocket 에러:', err);
-        socket.close(); // 오류 발생 시 연결을 닫습니다.
-      };
-
-      socket.onclose = () => {
-        console.log('WebSocket connection closed');
-      };
-
-      // 컴포넌트 언마운트 시 WebSocket 연결 종료
-      return () => {
-        socket.close();
-      };
-    };
-
-    fetchNotifications();
-  }, [authToken]);
-  // console.log(notifications);
-
-  // notifications.forEach((notification) => {
-  //   console.log('여기 notification 데이터 내용');
-  //   console.log(`id: ${notification.id}`);
-  //   console.log(`receiver: ${notification.receiver}`);
-  //   console.log(`sender: ${notification.sender}`);
-  //   console.log(`content: ${notification.content}`);
-  //   console.log(`lectureId: ${notification.lectureId}`);
-  //   console.log(`createdAt: ${notification.createdAt}`);
-  // });
 
   // 네비게이션 수정 예정
   const handleMoveToHome = () => {
